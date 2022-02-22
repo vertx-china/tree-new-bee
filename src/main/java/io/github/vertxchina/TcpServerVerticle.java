79cb34d5b758270461c5d0c65e026c6639c25176
chengenzhao <chengen.zhao@mail.mcgill.ca>
2022-02-22 16:13:07 +0800

    seems like 1st version is closed

--- src/main/java/io/github/vertxchina/TcpServerVerticle.java
+++ src/main/java/io/github/vertxchina/TcpServerVerticle.java
@@ -3,18 +3,41 @@ package io.github.vertxchina;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetSocket;
+import io.vertx.core.parsetools.RecordParser;
+
+import java.util.UUID;
 
 public class TcpServerVerticle extends AbstractVerticle {
   BiMap<String, NetSocket> idSocketBiMap = HashBiMap.create();
   @Override
   public void start() throws Exception {
-    super.start();
-
-    var netserver = vertx.createNetServer()
+    vertx.createNetServer()
         .connectHandler(socket ->{
+          var id = UUID.randomUUID().toString();
+          var json = new JsonObject().put("id", id);
+          idSocketBiMap.put(id, socket);
 
+          final var recordParser = RecordParser.newDelimited("\r\n", h -> {
+            try{
+              var messageJson = new JsonObject(h);
+              messageJson.put("id",id);
+              for(var receiverSocket:idSocketBiMap.values()){
+                if(!receiverSocket.equals(socket)){
+                  receiverSocket.write(messageJson +"\r\n");
+                }
+              }
+            }catch (Exception e){
+              e.printStackTrace();
+            }
+          }).maxRecordSize(1024*64);
+
+          socket.handler(recordParser);
+
+          socket.write(json.toString()+"\r\n");
+          socket.closeHandler((e)->idSocketBiMap.inverse().remove(socket));
         })
         .listen(32167, res ->{
           if(res.succeeded()){
@@ -23,10 +46,5 @@ public class TcpServerVerticle extends AbstractVerticle {
             System.out.println("netserver start failed");
           }
         });
-  }
-
-  @Override
-  public void stop() throws Exception {
-    super.stop();
   }
 }
