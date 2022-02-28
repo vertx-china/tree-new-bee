acb9f0a674d65659dad2e927d7c7260d06982e6f
chengenzhao <chengen.zhao@mail.mcgill.ca>
2022-02-28 14:00:17 +0800

    简单的，带有昵称和大厅用户列表的界面和功能

--- src/main/java/io/github/vertxchina/TcpServerVerticle.java
+++ src/main/java/io/github/vertxchina/TcpServerVerticle.java
@@ -3,18 +3,21 @@ package io.github.vertxchina;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.NetSocket;
 import io.vertx.core.parsetools.RecordParser;
 
-import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class TcpServerVerticle extends AbstractVerticle {
   DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z");
   BiMap<String, NetSocket> idSocketBiMap = HashBiMap.create();
+  Map<NetSocket, String> netSocketNicknameMap = new HashMap<>();
 
   @Override
   public void start() throws Exception {
@@ -30,8 +33,30 @@ public class TcpServerVerticle extends AbstractVerticle {
               var messageJson = new JsonObject(h);
               messageJson.put("id", id);
               messageJson.put("time", ZonedDateTime.now().format(dateFormatter));
-              for (var receiverSocket : idSocketBiMap.values()) {
-                receiverSocket.write(messageJson + "\r\n");
+
+              if(messageJson.containsKey("nickname")
+                  && messageJson.getValue("nickname") != null
+                  && !messageJson.getValue("nickname").toString().trim().isEmpty()){
+                var nickname = messageJson.getValue("nickname").toString().trim();
+                netSocketNicknameMap.put(socket, nickname);
+                var jsonArrays = new JsonArray();
+                for(var nn:netSocketNicknameMap.values()){
+                  jsonArrays.add(nn);
+                }
+                var jsonObject = new JsonObject().put("nicknames",jsonArrays);
+                for (var receiverSocket : idSocketBiMap.values()) {
+                  receiverSocket.write(jsonObject + "\r\n");
+                }
+              }
+
+              if(netSocketNicknameMap.containsKey(socket)){
+                messageJson.put("nickname",netSocketNicknameMap.get(socket));
+              }
+
+              if(messageJson.containsKey("message")){
+                for (var receiverSocket : idSocketBiMap.values()) {
+                  receiverSocket.write(messageJson + "\r\n");
+                }
               }
             } catch (Exception e) {
               e.printStackTrace();
@@ -41,7 +66,10 @@ public class TcpServerVerticle extends AbstractVerticle {
           socket.handler(recordParser);
 
           socket.write(json.toString() + "\r\n");
-          socket.closeHandler((e) -> idSocketBiMap.inverse().remove(socket));
+          socket.closeHandler((e) -> {
+            idSocketBiMap.inverse().remove(socket);
+            netSocketNicknameMap.remove(socket);
+          });
         })
         .listen(port, res -> {
           if (res.succeeded()) {
